19ba1929693aa1d0e41e00e1274ca42278d2c0aa
nguyen nam <16520775@gm.uit.edu.vn>
2021-02-25 21:47:06 +0700

    add

--- shop-backend/index.js
+++ shop-backend/index.js
@@ -1,23 +1,39 @@
-const express = require('express')
-const mongoose = require('mongoose');
+const express = require('express');
+
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const bcrypt = require('bcrypt');
-const userModel=require('./model/user.model');
-const session=require('express-session');
-const app = express();
 
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+const session=require('express-session');
+const jwt = require('jsonwebtoken');
+
+const userModel=require('./model/user.model');
+const cors = require("cors");
+
+const app = express();
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 const saltRounds = 10;
+
 mongoose.connect('mongodb://localhost/myshop', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
   useCreateIndex: true
 });
+
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.json()) // for parsing application/json;
 app.use(express.urlencoded({ extended: true }));
+
 app.use(
   session({
     key: "userId",
@@ -30,13 +46,29 @@ app.use(
   })
 );
 
+const verifyJwt=(req,res,next)=>{
+  const token= req.headers["x-access-token"];
+  if(!token){
+    res.send("need a token!")
+  }else
+    jwt.verify(token,"jwtsecret",(err,decoded)=>{
+      if(err){
+        res.json({auth:false,message:"you fail in authenticate !"})
+      }
+      else{
+        req.userId=decoded.id;
+        next();
+      }
+    })
+}
+
 app.post('/auth/register', function(req,res){
   const fisrtName=req.body.fisrtName;
   const lastName=req.body.lastName;
   const email=req.body.email;
-  const passWord=req.body.passWord;
+  const password=req.body.password;
 
-  bcrypt.hash(passWord, saltRounds, (err, hash) => {
+  bcrypt.hash(password, saltRounds, (err, hash) => {
     if (err) {
       console.log(err)
     }
@@ -49,37 +81,49 @@ app.post('/auth/register', function(req,res){
     data.save(function (err) {
       if (err) return handleError(err);
       // saved!
+      else
+      res.send('done');
     });
   })
 
 });
 app.get("/login", (req, res) => {
+  
   if (req.session.user) {
     res.send({ loggedIn: true, user: req.session.user });
   } else {
     res.send({ loggedIn: false });
   }
 });
+app.get("/useAuth",verifyJwt,(req,res)=>{
+  res.send("trang thanh toan");
+
+})
 app.post("/login",async(req,res)=>{
   const email= req.body.email;
   const password=req.body.password;
   var result= await userModel.find({email:email}); 
   console.log(result);
   if (result.length > 0) {
-    bcrypt.compare(password, result[0].password, (response) => {
+    bcrypt.compare(password, result[0].password, (err,response) => {
       if (response) {
+        
+        const id=result[0].id;
+        const token=jwt.sign({id},"jwtsecret",{
+          expiresIn:300,
+        });
         req.session.user = result;
-        console.log(req.session.user);
-        res.send(result);
+        res.json({auth:true, token:token, result:result});
       } else {
-        res.send({ message: "Wrong username/password combination!" });
+        res.json({auth:false,message: "Wrong username/password combination!"});
       }
     });
   } else {
-    res.send({ message: "User doesn't exist" });
+    res.json({auth:false, message: "User doesn't exist"});
+   
   }
 })
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
-app.listen(3000)
+app.listen(3001)
