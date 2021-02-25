ef02d3d3b2a6301b85d033c5ea9677236c172641
nguyen nam <16520775@gm.uit.edu.vn>
2021-02-25 21:52:28 +0700

    add

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,64 +1,101 @@
-import React,{useState,useEffect} from'react'
-import Axios from 'axios';
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import "../App.css";
 
-export default function Signup(){
-    Axios.defaults.withCredentials = true;
+export default function Registration() {
+  const [emailReg, setEmailReg] = useState("");
+  const [passwordReg, setPasswordReg] = useState("");
 
-    const [email,setEmail]=useState('');
-    const [password,setPassword]=useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [loginStatus, setLoginSatus]= useState('');
+  const [loginStatus, setLoginStatus] = useState(false);
 
-    const login=()=>{
-        Axios.post("http://localhost:3001/login",{
-        email:email,
-        password:password,
-        }).then((response)=>{
-            if(response.data.message)
-            {
-                setLoginSatus(response.data.message);
-                console.log(response.data)
-            }
-            else
-            {
-                setLoginSatus(response.data[0].email);
-                console.log(response.data[0])
-            }
-        });
-    
-    }
-    useEffect(() => {
-        
-        Axios.get("http://localhost:3001/login").then((response)=>{
-            if(response.data.loggedId==true){
-                setLoginSatus(response.data.user[0].email);
-                
-            }
-        });
-       
-    }, [])
-    return(
-        <>
-            <form className="login">
-                <h1>Login</h1>
-                <div className="email">
-                    <label>email</label>
-                    <input type="text"
-                    placeholder="email"
-                    onChange={(e) => {setEmail(e.target.value);
-                    }}
-                    />
-                </div>
-                <div className="password">
-                    <label>password</label>
-                    <input type="password"
-                     placeholder="password"
-                     onChange={(e)=>setPassword(e.target.value)}
-                     />
-                </div>
-                <button onClick={login}>Login</button>
-                
-            </form>
-        </>
-    )
+  Axios.defaults.withCredentials = true;
+
+  const isUseAuth=()=>{
+    Axios.get("http://localhost:3001/useAuth",{
+      headers:{
+        "x-access-token":localStorage.getItem("token")
+      },
+    }).then((response) =>{
+      console.log(response.data)
+    });
+  }
+
+  const register = () => {
+    Axios.post("http://localhost:3001/auth/register", {
+      email: emailReg,
+      password: passwordReg,
+    }).then((response) => {
+      console.log(response);
+    });
+  };
+
+  const login = () => {
+    Axios.post("http://localhost:3001/login", {
+      email: email,
+      password:password,
+    }).then((response) => {
+      if (response.data.auth) {
+        setLoginStatus(true);
+        localStorage.setItem("token",response.data.token)
+      } else {
+        setLoginStatus(false);
+      }
+    });
+  };
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/login").then((response) => {
+      if (response.data.loggedIn == true) {
+        setLoginStatus(true);
+      }
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="registration">
+        <h1>Registration</h1>
+        <label>Email</label>
+        <input
+          type="text"
+          onChange={(e) => {
+            setEmailReg(e.target.value);
+          }}
+        />
+        <label>Password</label>
+        <input
+          type="text"
+          onChange={(e) => {
+            setPasswordReg(e.target.value);
+          }}
+        />
+        <button onClick={register}> Register </button>
+      </div>
+
+      <div className="login">
+        <h1>Login</h1>
+        <input
+          type="text"
+          placeholder="Email..."
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <input
+          type="password"
+          placeholder="Password..."
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <button onClick={login}> Login </button>
+      </div>
+      {loginStatus&&
+      <button onClick={isUseAuth}>redirect</button>
+      }
+    </div>
+  );
 }
